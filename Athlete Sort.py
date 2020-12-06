c62d742a74ba33fe591bf890f772920d85691bd9
Teresa-09 <67002949+Teresa-09@users.noreply.github.com>
2020-12-06 15:27:25 +0530

    Create Athlete Sort.py

---
+++ Athlete Sort.py
@@ -0,0 +1,12 @@
+import math
+import os
+import random
+import re
+import sys
+
+n, m = map(int, input().split())
+rows = [input() for _ in range(n)]
+k = int(input())
+
+for row in sorted(rows, key=lambda row: int(row.split()[k])):
+    print(row)
